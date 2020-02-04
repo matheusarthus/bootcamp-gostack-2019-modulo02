ff76f3b2c2f6940a4c76d110d5a91ebb92c3ffa3
matheusarthus <matheus.arthus@gmail.com>
2020-02-04 14:10:09 -0300

    feature: Funcionalidade de Agendamentos concluída

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleControlle from './app/controllers/ScheduleControler';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,6 +25,9 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleControlle.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
